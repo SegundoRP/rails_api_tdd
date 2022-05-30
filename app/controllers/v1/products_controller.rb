fb613bf852c2c2c37f2089f6acd4878adb801b97
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-05-29 20:26:30 -0500

    test product update, test routing product index and pass them

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -2,11 +2,26 @@ module V1
   class ProductsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_store
+    before_action :set_product, only: %i[update]
+
+    def index
+
+    end
+
     def create
       @product = @store.products.new(product_params)
       if @product.valid?
         @product.save
         render :show, status: :created
+      else
+        render json: { errors: @product.errors.messages }, status: :bad_request
+      end
+    end
+
+
+    def update
+      if @product.update(product_params)
+        render :show, status: :ok
       else
         render json: { errors: @product.errors.messages }, status: :bad_request
       end
@@ -20,6 +35,11 @@ module V1
 
     def set_store
       @store = @current_user.store
+    end
+
+    def set_product
+      @product = @store.products.find_by(id: params[:id])
+      head :not_found unless @product
     end
   end
 end
--- spec/routing/product_spec.rb
+++ spec/routing/product_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
 describe 'Rutas de producto' do
-  it 'Ruta post de products' do
+  it 'Ruta POST de products' do
     expect(post: 'v1/products').to route_to(
       format: 'json',
       controller: 'v1/products',
       action: 'create'
+    )
+  end
+  it 'Ruta PUT para actualizar productos' do
+    expect(put: '/v1/products/1').to route_to(
+      format: 'json',
+      controller: 'v1/products',
+      action: 'update',
+      id: '1'
+    )
+  end
+  it 'Ruta index products' do
+    expect(get: '/v1/products').to route_to(
+      format: 'json',
+      controller: 'v1/products',
+      action: 'index'
     )
   end
 end
